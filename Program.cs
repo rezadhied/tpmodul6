af14f120747d4861eedecb1e8472a4357dde81ad
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2023-03-26 22:45:55 +0700

    implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -12,6 +12,17 @@ namespace tpmodul6_1302213016
             Console.WriteLine("\nIncreasing Play Count:");
             v.IncreasePlayCount(145);
             v.PrintVideoDetails();
+
+            SayaTubeVideo video1 = new SayaTubeVideo(null);
+
+            SayaTubeVideo video2 = new SayaTubeVideo("Video tes playcount");
+            for (int i = 0; i < 100; i++)
+            {
+                video2.IncreasePlayCount(9999999);
+            }
+            video2.PrintVideoDetails();
+
+
         }
     }
 }
--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,14 +16,37 @@ namespace tpmodul6_1302213016
 
         public SayaTubeVideo(string title)
         {
+            Debug.Assert(title != null && title.Length <= 100, 
+                "Judul video harus memiliki panjang maksimal 100 karakter dan tidak berupa null");
+         
             this.id = new Random().Next(100, 999);
+            try
+            {
+                this.title = checked(title);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
             this.title = title;
             this.playCount = 0;
         }
 
         public void IncreasePlayCount(int count)
         {
-            this.playCount += count;
+            Debug.Assert(count <= 10000000);
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (System.OverflowException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
 
         public void PrintVideoDetails()
