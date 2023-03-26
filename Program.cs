84aca31035b5c9d410d69ef94944aed9a1bd1a59
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2023-03-26 20:12:05 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul6_1302213016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo v = new SayaTubeVideo("Tutorial Design By Contract - Reza Adhie Dharmawan");
+            v.PrintVideoDetails();
+
+            Console.WriteLine("\nIncreasing Play Count:");
+            v.IncreasePlayCount(145);
+            v.PrintVideoDetails();
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302213016
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.id = new Random().Next(100, 999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video Details:");
+            Console.WriteLine("ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Play Count: " + this.playCount);
+        }
+    }
+
+    
+
+
+}
